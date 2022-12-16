e8e217e3061c183f2e912081057a650901aeb5f0
Isadora <isadoravanco@gmail.com>
2022-12-16 11:44:25 -0300

    16/12

--- cp5.c
+++ cp5.c
@@ -1,17 +1,24 @@
 // Baseado no livro 'Introdução a C' de Mauricio Ariche 
 #include <stdio.h> 
+#include <stdlib.h>
+#include <time.h>
 
 int main(){ 
-    int numerosecreto = 42;
-    int chute;
-    int pontos = 1000;
-    int tentativas = 1;
-
     printf("************************************\n");
     printf("* Bem vindo ao Jogo da Adivinhacao *\n");
     printf("************************************\n");
 
-    while(1){
+    int chute, maior;
+    int acertou = 0;
+    int tentativas = 1;
+    double pontosperdidos;
+    double pontos = 1000;
+
+    srand(time(0)); //define a "semente" para a função randomica, ou seja, como se fosse uma 
+    //"fórmula" diferente para cada jogo, fazendo ser o mais randomico possivel.
+    int numerosecreto = rand() % 100; //sorteia um numero deixa no intervalo de 0 a 99
+
+    while(!acertou){
         printf("Qual e o seu %do chute? ", tentativas);
         scanf("%d", &chute);
 
@@ -22,19 +29,22 @@ int main(){
 
         printf("Seu %do chute foi %d\n", tentativas, chute);
         
-        int acertou = chute == numerosecreto;
-        int maior = chute > numerosecreto;
+        acertou = chute == numerosecreto;
+        maior = chute > numerosecreto;
     
         if(acertou) {
            printf("Parabens! Voce acertou!\n");
-           break;
         }else if(maior){
             printf("Seu chute foi maior do que o numero secreto!\n");
-            
         }else{
             printf("Seu chute foi menor do que o numero secreto!\n");
         }
 
         tentativas++;
+        pontosperdidos = abs((chute - numerosecreto)) / 2.0;
+        pontos = pontos - pontosperdidos;
     }
+
+    printf("Voce fez %.2lf pontos.\n", pontos);
+    printf("Obrigada por jogar! ;)\n");
 }
