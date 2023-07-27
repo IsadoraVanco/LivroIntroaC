7ad7143d36e11c3249504b981e593e6879011db8
Isadora <isadoravanco@gmail.com>
2023-07-27 01:00:26 -0300

    cp20 finalizado

---
+++ jogoFogeFoge/capitulos/cp20-ui.h
@@ -0,0 +1,11 @@
+// Baseado no livro 'Introdução à programação em C: 
+// Os primeiros passos de um desenvolvedor' de Maurício Aniche 
+#ifndef _UI_H_
+#define _UI_H_
+
+#include "cp20-mapa.h"
+
+void imprimeParte(char desenho[4][7], int parte);
+void imprimeMapa(MAPA* m);
+
+#endif
